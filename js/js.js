48df7a15931f28952d1f020b5a77080771331381
Hans M. Boron <hansmateusboron@gmail.com>
2022-01-11 10:02:56 -0300

    create game loop

--- js/js.js
+++ js/js.js
@@ -5,6 +5,19 @@ function start() {
     $("#fundoGame").append("<div id='inimigo1' class='anima-helicopter'></div>");
     $("#fundoGame").append("<div id='inimigo2'></div>");
     $("#fundoGame").append("<div id='amigo' class='anima-friend'></div>");
+
+    var jogo = {}
+
+    //Game Loop
+    jogo.timer = setInterval(loop, 60);
+    function loop() {
+        movefundo();
+    }
+
+    function movefundo() {
+        esquerda = parseInt($("#fundoGame").css("background-position"));
+        $("#fundoGame").css("background-position", esquerda - 2);
+    }
 }
 
 
