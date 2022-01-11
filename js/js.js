d1d093f21707d82640987726da17b4355faf6ea6
Hans M. Boron <hansmateusboron@gmail.com>
2022-01-11 09:57:08 -0300

    animate images

--- js/js.js
+++ js/js.js
@@ -1,9 +1,10 @@
 function start() {
     $("#inicio").hide();
 
-    $("#fundoGame").append("<div id='jogador'></div>");
-    $("#fundoGame").append("<div id='inimigo1'></div>");
+    $("#fundoGame").append("<div id='jogador' class='anima-apache'></div>");
+    $("#fundoGame").append("<div id='inimigo1' class='anima-helicopter'></div>");
     $("#fundoGame").append("<div id='inimigo2'></div>");
-    $("#fundoGame").append("<div id='amigo'></div>");
-
+    $("#fundoGame").append("<div id='amigo' class='anima-friend'></div>");
 }
+
+
