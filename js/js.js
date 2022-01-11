f451375d6fde4290c4cbe10782691d3a45647d3b
Hans M. Boron <hansmateusboron@gmail.com>
2022-01-11 11:08:46 -0300

    move player up and down

--- js/js.js
+++ js/js.js
@@ -8,15 +8,56 @@ function start() {
 
     var jogo = {}
 
+    var TECLA = {
+        W: 87,
+        S: 83,
+        D: 68
+    }
+
+    jogo.pressionou = [];
+
+    //Verifica se o usuário pressionou alguma tecla	
+
+    $(document).keydown(function (e) {
+        jogo.pressionou[e.which] = true;
+    });
+
+
+    $(document).keyup(function (e) {
+        jogo.pressionou[e.which] = false;
+    });
+
     //Game Loop
     jogo.timer = setInterval(loop, 60);
     function loop() {
         movefundo();
+        movejogador();
     }
 
     function movefundo() {
         esquerda = parseInt($("#fundoGame").css("background-position"));
         $("#fundoGame").css("background-position", esquerda - 2);
+    }
+
+    function movejogador() {
+
+        if (jogo.pressionou[TECLA.W]) {
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo - 10);
+
+        }
+
+        if (jogo.pressionou[TECLA.S]) {
+
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo + 10);
+        }
+
+        if (jogo.pressionou[TECLA.D]) {
+
+            //Chama função Disparo	
+        }
+
     }
 }
 
