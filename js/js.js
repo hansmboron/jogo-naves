23e9723d2dde5a19fbcd2d9011e85fdeb7ac9a48
Hans M. Boron <hansmateusboron@gmail.com>
2022-01-11 11:20:43 -0300

    move helicopter

--- js/js.js
+++ js/js.js
@@ -7,6 +7,8 @@ function start() {
     $("#fundoGame").append("<div id='amigo' class='anima-friend'></div>");
 
     var jogo = {}
+    var velocidade = 5;
+    var posicaoY = parseInt(Math.random() * 334);
 
     var TECLA = {
         W: 87,
@@ -17,7 +19,6 @@ function start() {
     jogo.pressionou = [];
 
     //Verifica se o usuário pressionou alguma tecla	
-
     $(document).keydown(function (e) {
         jogo.pressionou[e.which] = true;
     });
@@ -32,6 +33,7 @@ function start() {
     function loop() {
         movefundo();
         movejogador();
+        moveinimigo1();
     }
 
     function movefundo() {
@@ -40,24 +42,41 @@ function start() {
     }
 
     function movejogador() {
-
         if (jogo.pressionou[TECLA.W]) {
             var topo = parseInt($("#jogador").css("top"));
             $("#jogador").css("top", topo - 10);
-
+            if (topo <= 0) {
+                $("#jogador").css("top", topo + 10);
+            }
         }
 
         if (jogo.pressionou[TECLA.S]) {
 
             var topo = parseInt($("#jogador").css("top"));
             $("#jogador").css("top", topo + 10);
+            if (topo >= 434) {
+                $("#jogador").css("top", topo - 10);
+            }
         }
 
         if (jogo.pressionou[TECLA.D]) {
 
             //Chama função Disparo	
         }
+    }
 
+    function moveinimigo1() {
+
+        posicaoX = parseInt($("#inimigo1").css("left"));
+        $("#inimigo1").css("left", posicaoX - velocidade);
+        $("#inimigo1").css("top", posicaoY);
+
+        if (posicaoX <= 0) {
+            posicaoY = parseInt(Math.random() * 334);
+            $("#inimigo1").css("left", 694);
+            $("#inimigo1").css("top", posicaoY);
+
+        }
     }
 }
 
